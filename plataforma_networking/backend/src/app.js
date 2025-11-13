eb8575cde31645a36112263bf8ddb67b241c0ecf
Joao Secundo <alundra@academico.ufs.br>
2025-11-13 10:51:51 -0300

    Permitir CORS e atualizar porta da aplicação

--- plataforma_networking/backend/src/app.js
+++ plataforma_networking/backend/src/app.js
@@ -3,8 +3,16 @@ import express from "express"
 import rotas from "./rotas/index.js"
 import rotaNotFound from "./middlewares/rotaNotFound.js"
 import tratadorDeErros from "./middlewares/tratadorDeErros.js"
+import cors from "cors"
 
 const app = express()
+
+// aplicar cors
+app.use(cors({
+    origin: "http://localhost:3000",
+    methods: "*",
+    allowedHeaders: "*"
+}))
 
 // Registrar (middleware) que ligará as rotas aos métodos dos controllers
 rotas(app)
